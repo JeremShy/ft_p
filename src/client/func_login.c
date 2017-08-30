bd266f11c46166b0aa4f7181d0c946b98d276a3c
jcamhi <jeremshy@h3lp.fr>
2017-08-30 15:48:15 +0200

    src/client/func_login.c

--- src/client/func_login.c
+++ src/client/func_login.c
@@ -1,14 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   func_login.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcamhi <jcamhi@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/30 15:46:10 by jcamhi            #+#    #+#             */
+/*   Updated: 2017/08/30 15:48:09 by jcamhi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <client.h>
 
-//TODO: Free answer dans les return
+static void	fln(t_data *data, char *password)
+{
+	char	*send;
 
-void	func_login(t_data *data)
+	send = build_request(NAME_RFC_PASS, password);
+	write(data->socket, send, ft_strlen(send));
+	free(send);
+}
+
+static void	pass_part(t_data *data, char *password)
+{
+	t_answer answer;
+
+	fln(data, password);
+	answer = get_answer(data);
+	if (answer.error <= 0)
+		error_connection(data);
+	if (answer.code == 530)
+	{
+		ft_printf("%s\n", answer.str);
+		free_answer(answer);
+		return (set_error_and_ret(data, NULL, NULL));
+	}
+	else if (answer.code == 230)
+	{
+		free_answer(answer);
+		return (pem(NULL, "Login successful."));
+	}
+	else
+	{
+		ft_printf("Error: %s\n", answer.commentaire);
+		free_answer(answer);
+		return (set_error_and_ret(data, NULL, NULL));
+	}
+}
+
+static int	check_cases(t_data *data, t_answer answer, char *password)
+{
+	if (answer.error <= 0)
+		error_connection(data);
+	if (answer.code == 331 && !password)
+	{
+		ft_printf("%s\n", answer.str);
+		data->error = 1;
+		free_answer(answer);
+		return (1);
+	}
+	else if (answer.code == 230)
+	{
+		ft_putstr("Login successful.\n");
+		free_answer(answer);
+		return (1);
+	}
+	else if (answer.code != 331)
+	{
+		ft_printf("%s\n", answer.str);
+		data->error = 1;
+		free_answer(answer);
+		return (1);
+	}
+	return (0);
+}
+
+void		func_login(t_data *data)
 {
 	t_answer	answer;
-	t_cmd	*cmd;
-	char	*send;
-	char	*username;
-	char	*password;
+	t_cmd		*cmd;
+	char		*send;
+	char		*username;
+	char		*password;
 
 	cmd = &(data->cmd);
 	if (cmd->cmd_tab[1] == NULL)
@@ -25,53 +98,7 @@ void	func_login(t_data *data)
 	write(data->socket, send, ft_strlen(send));
 	free(send);
 	answer = get_answer(data);
-	if (answer.error <= 0)
-		error_connection(data);
-	if (answer.code == 331 && !password)
-	{
-		ft_printf("%s\n", answer.str);
-		data->error = 1;
+	if (check_cases(data, answer, password))
 		free_answer(answer);
-		return ;
-	}
-	else if (answer.code == 230)
-	{
-		ft_putstr("Login successful.\n");
-		free_answer(answer);
-		return ;
-	}
-	else if (answer.code != 331)
-	{
-		ft_printf("%s\n", answer.str);
-		data->error = 1;
-		free_answer(answer);
-		return ;
-	}
-	send = build_request(NAME_RFC_PASS, password);
-	write(data->socket, send, ft_strlen(send));
-	free(send);
-	free_answer(answer);
-	answer = get_answer(data);
-	if (answer.error <= 0)
-		error_connection(data);
-	if (answer.code == 530)
-	{
-		ft_printf("%s\n", answer.str);
-		data->error = 1;
-		free_answer(answer);
-		return ;
-	}
-	else if (answer.code == 230)
-	{
-		ft_putstr("Login successful.\n");
-		free_answer(answer);
-		return ;
-	}
-	else
-	{
-		ft_printf("Error : %s\n", answer.commentaire);
-		free_answer(answer);
-		data->error = 1;
-		return ;
-	}
+	pass_part(data, password);
 }
