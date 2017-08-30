5ef23ee8547de975a190c0a1c3b924d4123ec704
jcamhi <jeremshy@h3lp.fr>
2017-08-30 16:05:44 +0200

    src/client/func_type.c

--- src/client/func_type.c
+++ src/client/func_type.c
@@ -1,23 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   func_type.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcamhi <jcamhi@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/30 16:02:20 by jcamhi            #+#    #+#             */
+/*   Updated: 2017/08/30 16:05:03 by jcamhi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <client.h>
+#define ER "type {{I || IMAGE || B || BINARY} || {A || ASCII}}"
 
 void	func_type(t_data *data)
 {
-	char	*send;
-	char	*arg;
-	char	*str;
+	char		*send;
+	char		*arg;
+	char		*str;
 	t_answer	answer;
 
 	str = (data->cmd).cmd_tab[1];
-	if (ft_strequ(str, "IMAGE") || ft_strequ(str, "BINARY") || ft_strequ(str, "I") || ft_strequ(str, "B"))
+	if (ft_strequ(str, "IMAGE") || ft_strequ(str, "BINARY")
+			|| ft_strequ(str, "I") || ft_strequ(str, "B"))
 		arg = ft_strdup("I");
 	else if (ft_strequ(str, "ASCII") || ft_strequ(str, "A"))
 		arg = ft_strdup("A");
 	else
-	{
-		ft_printf("Usage: type {{I || IMAGE || B || BINARY} || {A || ASCII}}\n");
-		data->error = 1;
-		return ;
-	}
+		return (set_error_and_ret(data, "Usage", ER));
 	send = build_request(NAME_RFC_TYPE, arg);
 	free(arg);
 	write(data->socket, send, ft_strlen(send));
