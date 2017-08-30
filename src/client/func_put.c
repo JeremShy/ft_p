56db0370e32b614ee3062cf69f4c7edd214a4e42
jcamhi <jeremshy@h3lp.fr>
2017-08-30 16:01:14 +0200

    src/client/func_put.c

--- src/client/func_put.c
+++ src/client/func_put.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   func_put.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcamhi <jcamhi@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/30 15:55:12 by jcamhi            #+#    #+#             */
+/*   Updated: 2017/08/30 16:00:59 by jcamhi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <client.h>
 
 static int	get_fd(t_data *data, char *file)
@@ -13,30 +25,43 @@ static int	get_fd(t_data *data, char *file)
 	return (fd);
 }
 
-void	func_put(t_data *data)
+static int	begin(t_data *data, int *data_socket, int *fd)
 {
-	int	data_socket;
-	char	*send;
-	t_answer	answer;
-	int	fd;
-
 	if (!data->cmd.cmd_tab[1] || !data->cmd.cmd_tab[2])
 	{
- 		ft_printf("Usage: put local remote\n");
+		ft_printf("Usage: put local remote\n");
 		data->error = 1;
-		return ;
+		return (1);
 	}
-	data_socket = set_passive_mode(data);
+	*data_socket = set_passive_mode(data);
 	if (data->error)
 	{
-		ft_printf("Error while trying to open a passive connection\n");
-		return;
+		ft_printf("Error while trying to open a passive connection");
+		return (1);
 	}
-	if ((fd = get_fd(data, (data->cmd).cmd_tab[1])) == -1)
+	if ((*fd = get_fd(data, (data->cmd).cmd_tab[1])) == -1)
 	{
-		close(data_socket);
-		return ;
+		close(*data_socket);
+		return (1);
 	}
+	return (0);
+}
+
+static void	fln(t_data *data, t_answer answer)
+{
+	free_answer(answer);
+	return (set_error_and_ret(data, NULL, NULL));
+}
+
+void		func_put(t_data *data)
+{
+	int			data_socket;
+	char		*send;
+	t_answer	answer;
+	int			fd;
+
+	if (begin(data, &data_socket, &fd))
+		return ;
 	send = build_request(NAME_RFC_PUT, (data->cmd).cmd_tab[2]);
 	write(data->socket, send, ft_strlen(send));
 	free(send);
@@ -45,11 +70,7 @@ void	func_put(t_data *data)
 		error_connection(data);
 	ft_printf("%s\n", answer.str);
 	if (answer.code / 100 > 2)
-	{
-		data->error = 1;
-		free_answer(answer);
-		return ;
-	}
+		return (fln(data, answer));
 	redirect_data(fd, data_socket);
 	close(data_socket);
 	free_answer(answer);
